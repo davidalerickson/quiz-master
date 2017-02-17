8fa14ca53880c2bc0fb7c9e2d697555811366768
davidalerickson <davidalerickson@hotmail.com>
2017-02-17 10:04:13 -0500

    added render function and some styling

--- app.js
+++ app.js
@@ -20,18 +20,40 @@ var Quiz = {
 
             ],
 
-
-
   currentCorrect: 0
 }
 
+//State Modification functions
+var numCorrect = function(Quiz){
+  Quiz.currentCorrect++;
+};
+
+//render functions
+var renderQuestion = function(Quiz, element){
+  var questionHTML  = `
+  <h1 class = "question js-question">${Quiz.questions[1].question}</h1>
+  <input type="radio" class="radioAns js-radio1" name="answer" value="Answer 1"><span class ="answer-text js-answer-text">${Quiz.questions[1].answers[0]}</span><br>
+  <input type="radio" class="radioAns js-radio2" name="answer" value="Answer 2"><span class ="answer-text js-answer-text">${Quiz.questions[1].answers[1]}</span><br>
+  <input type="radio" class="radioAns js-radio3" name="answer" value="Answer 3"><span class ="answer-text js-answer-text">${Quiz.questions[1].answers[2]}</span><br>
+  <input type="radio" class="radioAns js-radio4" name="answer" value="Answer 4"><span class ="answer-text js-answer-text">${Quiz.questions[1].answers[3]}</span><br>`;
+
+  element.html(questionHTML);
+}
+
+
 $(function(){
+
   $(".js-question").text(Quiz.questions[0].question);
 
   $("#js-startButton").on('click', function(event){
     $(".js-question-body").toggleClass("hidden");
     $(".js-welcome-screen").toggleClass("hidden");
   });
+
+  function loadNextQuestion(){
+    renderQuestion(Quiz, $(".js-question-block"))
+  }
+  loadNextQuestion();
 });
 // alert(Quiz.questions[0].question);
 // alert(Quiz.questions[0].correctAnswer);
